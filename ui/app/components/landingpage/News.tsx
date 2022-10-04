d48ec0219ba2e2e819b4e24798b695a631bc8af5
Jan Lauber <jan.lauber@protonmail.ch>
2022-10-04 20:25:05 +0200

    change news when no news
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- ui/app/components/landingpage/News.tsx
+++ ui/app/components/landingpage/News.tsx
@@ -8,6 +8,24 @@ interface Props {
 
 const News = (props: Props) => {
 
+    if (props.news.length === 0) {
+        return (
+            <div className="relative bg-white px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28 -z-10">
+                <div className="absolute inset-0">
+                    <div className="h-1/3 bg-white sm:h-2/3" />
+                </div>
+                <div className="relative mx-auto max-w-7xl">
+                    <div className="text-center">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Neuigkeiten</h2>
+                        <p className="mx-auto mt-3 max-w-2xl text-xl text-gray-500 sm:mt-4">
+                            Derzeit haben wir keine Neuigkeiten.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="relative bg-gray-50 px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28 -z-10">
             <div className="absolute inset-0">
